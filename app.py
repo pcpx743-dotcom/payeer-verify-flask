4f2bb12cf40f0d413958c8723065640308b22cfe
pcpx743-dotcom <pcpx743@gmail.com>
2025-09-03 16:28:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, Response
+import os
 
 app = Flask(__name__)
 
-TOKEN = "payeer_2256083421"       # fayl ichidagi aniq matn
+TOKEN = "payeer_2256083421"
 FILENAME = "payeer_2256083421.txt"
 
 @app.get("/")
@@ -14,7 +15,6 @@ def verify():
     return Response(TOKEN, mimetype="text/plain")
 
 if __name__ == "__main__":
-    # Render avtomatik PORT o'zgaruvchisidan foydalanadi,
-    # lekin lokalda ham ishga tushadi.
-    import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Render PORT muhit o'zgaruvchisini oladi
+    app.run(host="0.0.0.0", port=port)
+
